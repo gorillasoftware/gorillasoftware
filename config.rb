369bc92bfef6e42f011e88d23a095a914750c5a8
sebastian de castelberg <github.com@kpricorn.org>
2011-06-12 15:15:59 +0200

    minify css/js

--- config.rb
+++ config.rb
@@ -41,20 +41,20 @@ end
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
   
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
   
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
   
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
   
   # Compress PNGs after build (gem install middleman-smusher)
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Generate ugly/obfuscated HTML from Haml
   # activate :ugly_haml
