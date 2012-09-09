4e06e591ed7dd9e762af7d6f441e189fec070438
Sebastian de Castelberg <sebu@kpricorn.org>
2012-09-09 21:20:51 +0200

    title and description

--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -1,18 +1,20 @@
+# encoding: UTF-8
+
 module SiteHelpers
 
   def page_title
-    title = "Set your site title in /helpers/site_helpers.rb"
+    title = "GORILLA software"
     if data.page.title
       title << " | " + data.page.title
     end
     title
   end
-  
+
   def page_description
     if data.page.description
       description = data.page.description
     else
-      description = "Set your site description in /helpers/site_helpers.rb"
+      description = "GORILLA software. Individuelle Lösungen fürs Web."
     end
     description
   end
