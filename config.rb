9ce0bfdeae26f5e5e0dafc06df2c86f96fa5bc02
Sebastian de Castelberg <sebu@kpricorn.org>
2013-09-03 00:25:07 +0200

    cleanup

--- config.rb
+++ config.rb
@@ -1,28 +1,13 @@
-activate :livereload
-
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
 activate :blog do |blog|
   blog.prefix = "blog"
-  # blog.permalink = ":year/:month/:day/:title.html"
-  # blog.sources = ":year-:month-:day-:title.html"
-  # blog.taglink = "tags/:tag.html"
   blog.layout = "layouts/blog"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = ":year.html"
-  # blog.month_link = ":year/:month.html"
-  # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
-
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
-
   blog.paginate = true
-  blog.per_page = 3
-  # blog.page_link = "page/:num"
 end
 
 activate :syntax, lineanchors: 'line'
@@ -35,6 +20,7 @@ set :haml, { ugly: true }
 page "/feed.xml", :layout => false
 
 activate :directory_indexes
+activate :livereload
 
 # Build-specific configuration
 configure :build do
